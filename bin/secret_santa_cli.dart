30d9a763eb6c41e434866f7130f6f097fcd736be
Michael Bernd Beier <code@michaelberndbeier.com>
2021-09-26 16:09:42 +0200

    .

--- bin/secret_santa_cli.dart
+++ bin/secret_santa_cli.dart
@@ -2,6 +2,7 @@ import 'package:secret_santa_cli/secretSanta.dart';
 
 void main(List<String> arguments) {
 	
+  print("Secret Santa started");
 	var senders = getSenders(getParticipants());
 	var pairings = getPairings(senders);
 	printPairings(pairings);
